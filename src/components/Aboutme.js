56ba81b0ea7ae67a9d6cffc30c0b4f90d594cea2
unknown <shabankhawar99@gmail.com>
2020-04-10 04:17:57 +0500

    succesfuly fetched profile picture

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Aboutme() {
-  axios.get("www.api-github.com/users/ShabanK").then((data) => {
-    console.log(data);
-  });
-  return <></>;
+  const [profilePicture, setProfilePicture] = useState("");
+  async function fetchPicture() {
+    await axios.get("https://api.github.com/users/ShabanK").then((obj) => {
+      setProfilePicture(obj.data.avatar_url);
+    });
+  }
+  useEffect(() => {
+    fetchPicture();
+  }, []);
+  //
+  return (
+    <>
+      <img src={profilePicture} alt="well i guess axios failed me" />
+    </>
+  );
 }
 
 export default Aboutme;
